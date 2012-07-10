866ec79dfb9074ddef16ede9b7aa2150b8a6b23d
Martijn Meijers <b.m.meijers@tudelft.nl>
2012-07-10 16:19:34 +0200

    - Release 0.0.1: first public release

--- src/connection/connect.py
+++ src/connection/connect.py
@@ -1,9 +1,32 @@
 import os
 import logging
-from ConfigParser import ConfigParser
+from ConfigParser import ConfigParser, NoOptionError
 from psycopg2 import connect
 
-def _dsn():
+def auth_params():
+    config = os.environ.get('DBCONFIG', 'localhost')
+    logging.debug("DBCONFIG: {}".format(config))
+    path = os.path.dirname(__file__)
+    file_nm = os.path.join(path, 
+        os.path.join(
+            os.pardir,
+            os.path.join("config","{}.ini".format(config))))
+    configparser = ConfigParser()
+    configparser.read(file_nm)
+    try:
+        sslmode = configparser.get('database', 'sslmode')
+    except NoOptionError:
+        sslmode = "prefer"
+    auth = {
+        "database": configparser.get('database', 'database'),
+        "username": configparser.get('database', 'username'),
+        "password": configparser.get('database', 'password'),
+        "host": configparser.get('database', 'host'),
+        "port": configparser.get('database', 'port'),
+        "sslmode": sslmode,}
+    return auth
+
+def dsn():
     # PostgreSQL has the following parameters for connections:
     # dbname - the database name (only in dsn string)
     # database - the database name (only as keyword argument)
@@ -12,41 +35,28 @@ def _dsn():
     # host - database host address (defaults to UNIX socket if not provided)
     # port - connection port number (defaults to 5432 if not provided)
     # sslmode - SSL TCP/IP negotiation mode
-    configparser = ConfigParser()
-    config = os.environ.get('DBCONFIG', 'localhost')
-    logging.debug("DBCONFIG: {}".format(config))
-    path = os.path.dirname(__file__)
-    file_nm = os.path.join(path, 
-        os.path.join(
-            os.pardir,
-            os.path.join("config","{}.ini".format(config))))
-
-    configparser.read(file_nm)
-    try:
-        sslmode = configparser.get('database', 'sslmode')
-    except ConfigParser.NoOptionError:
-        sslmode = "prefer"
+    auth = auth_params()
     dsn = "dbname={} user={} password={} host={} port={} sslmode={}"
     logging.debug("Connecting to: {}".format(
         dsn.format(
-            configparser.get('database', 'database'),
-            configparser.get('database', 'username'),
+            auth['database'],
+            auth['username'],
             '********', # hide password
-            configparser.get('database', 'host'),
-            configparser.get('database', 'port'),
-            sslmode,)))
+            auth['host'],
+            auth['port'],
+            auth['sslmode'],)))
     return dsn.format(
-        configparser.get('database', 'database'),
-        configparser.get('database', 'username'),
-        configparser.get('database', 'password'),
-        configparser.get('database', 'host'),
-        configparser.get('database', 'port'),
-        sslmode,)
+        auth['database'],
+        auth['username'],
+        auth['password'],
+        auth['host'],
+        auth['port'],
+        auth['sslmode'],)
 
 
 class ConnectionFactory(object):
     def __init__(self):
-        self.dsn = _dsn()
+        self.dsn = dsn()
     
     def connection(self):
         return connect(self.dsn)
