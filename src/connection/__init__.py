18d1e8c7dc67b6cbafaa03bc121c49050bc610a3
Martijn Meijers <b.m.meijers@tudelft.nl>
2013-02-07 20:42:32 +0100

    v0.0.2

--- src/connection/__init__.py
+++ src/connection/__init__.py
@@ -1,6 +1,6 @@
 # created: 26 jun 2012, MM
 """Helper module for setting up a Database connection to PostgreSQL"""
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Martijn Meijers"
 __license__ = "MIT License"
